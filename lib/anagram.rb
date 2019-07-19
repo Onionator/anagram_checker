6b0ca4bec31efec2206ddf291c184b478e5047dc
Sam Cook <WhenPotatoesFry@gmail.com>
2019-07-19 09:12:46 -0700

    pass a test if the 2 words passed to it are the same length with all special characters and punctuation removed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,9 +2,15 @@ require 'pry'
 
 class String
   def anagram?(word)
+    delete!("!@#${%^&*()}_+=[]|:;\"'<,>.?/")
+    word.delete!("!@#${%^&*()}_+=[]|:;\"'<,>.?/")
+    if self.length == word.length
     word.each_char { |chr|
       sub!(word[chr], "")
     }
+  else
+    return false
+  end
     if self == ""
       return true
     else
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,4 +5,7 @@ describe('anagram?') do
   it('will return true if the 2 words passed to it are agagrams') do
     expect("star".anagram?("rats")).to(eq(true))
   end
+  it('will return true if the 2 words passed to it are the same length with all special characters and punctuation removed') do
+    expect("lame".anagram?("meals")).to(eq(false))
+  end
 end
