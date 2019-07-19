5b95432bf7d310b4d972add7ec969da96fd7ebdd
Sam Cook <WhenPotatoesFry@gmail.com>
2019-07-19 09:04:16 -0700

    pass the test when  star and rats are entered

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 require 'pry'
+
+class String
+  def anagram?(word)
+    word.each_char { |chr|
+      sub!(word[chr], "")
+    }
+    if self == ""
+      return true
+    else
+      return false
+    end
+
+  end
+end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,2 +1,8 @@
 require 'rspec'
 require 'anagram'
+
+describe('anagram?') do
+  it('will return true if the 2 words passed to it are agagrams') do
+    expect("star".anagram?("rats")).to(eq(true))
+  end
+end
