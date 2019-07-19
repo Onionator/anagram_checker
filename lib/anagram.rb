381e6575ee2182ed875c4a934e90e89258191cdf
Sam Cook <WhenPotatoesFry@gmail.com>
2019-07-19 10:57:07 -0700

    take inputs and output a sentence to the terminal if the words contain the same letters, but are not anagrams.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -18,8 +18,24 @@ class Anagram
     elsif word_holder == @first_word
       return "These words are antigrams"
     else
-      return false
+      word_holder.each_char { |chr|
+        @first_word.sub!(word_holder[chr], "")
+      }
+      output = ""
+      @first_word.each_char.with_index { |chr, i|
+        if i == @first_word.length - 1 && output.length > 1
+          output += "and "
+        end
+        output += chr
+        if i < @first_word.length - 1
+          output += ", "
+        end
+      }
+      output += "."
+      puts "These phrases aren't anagrams, but they both contain: #{output}"
+        return false
     end
-
   end
 end
+
+p Anagram.new(gets.chomp).anagram?(gets.chomp)
